ad7fb3c31b5e80ace39fc3e906dc46faafe7c563
Toshiki Nakamura <ee193014@meiji.ac.jp>
2023-11-14 20:01:51 +0900

    Fix

--- src/motion_decision.cpp
+++ src/motion_decision.cpp
@@ -650,6 +650,9 @@ void MotionDecision::process()
             std::cout << "stop : (" << (auto_flag ? "auto" : "manual") << ")"<< std::endl;
             vel.linear.x = 0.0;
             vel.angular.z = 0.0;
+
+            stuck_count = 0;
+            trigger_count = 0;
         }
         if(emergency_stop_flag){
             std::cout << "emergency stop" << std::endl;
