bff0c4ff311dd1a6155b1ace867f2dce8a403538
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-07-14 21:36:23 +0900

    Fix format

--- include/motion_decision/motion_decision.h
+++ include/motion_decision/motion_decision.h
@@ -16,18 +16,18 @@
 #include <sensor_msgs/LaserScan.h>
 #include <std_msgs/Bool.h>
 #include <string>
+#include <utility>
 
 struct MotionDecisionParams
 {
   int hz;
-  int recovery_mode_threshold;
-  int trigger_count_threshold;
-  float max_speed;
+  float max_velocity;
   float max_yawrate;
   float dt;
   float predict_time;
   float collision_distance;
   float safety_collision_time;
+  float stuck_time_threshold;
   std::string stop_sound_path;
   std::string recovery_sound_path;
   std::string task_stop_sound_path;
@@ -35,36 +35,32 @@ struct MotionDecisionParams
 
 struct RecoveryParams
 {
+  bool use = true;
   float max_velocity;
   float max_yawrate;
   float velocity_resolution;
   float yawrate_resolution;
+  float time;
 };
 
 struct Flags
 {
   bool emergency_stop = false;
   bool task_stop = false;
-  bool auto_mode = false;
-  bool move_mode = false;
-  bool joy = false;
   bool intersection = false;
-  bool safety_mode = false;
   bool local_path_received = false;
-  bool enable_recovery_mode = true;
-  bool recovery_mode = false;
 };
 
 struct Counters
 {
   int stuck = 0;
-  int trigger = 0;
+  int recovery = 0;
 };
 
 struct LaserInfo
 {
-  int front_min_idx = 0;
-  int rear_min_idx = 0;
+  int front_index_of_min_range = 0;
+  int rear_index_of_min_range = 0;
   float front_min_range = -1.0;
   float rear_min_range = -1.0;
 };
@@ -96,11 +92,10 @@ private:
    * @brief Emergency stop flag callback function
    * @param [in] msg Msg from emergency_stop_flag_sub_
    */
-  void emergency_stop_flag_callback(const std_msgs::BoolConstPtr &msg);
+  void emergency_stop_flag_callback(const std_msgs::BoolConstPtr &msg) { flags_.emergency_stop = msg->data; }
 
   /**
    * @brief Front laser callback function
-   * @details Cache the closest valid sensor data and its index in front_min_range and front_min_idx respectively
    * @param [in] msg Msg from front_laser_sub_
    */
   void front_laser_callback(const sensor_msgs::LaserScanConstPtr &msg);
@@ -113,20 +108,19 @@ private:
   void joy_callback(const sensor_msgs::JoyConstPtr &msg);
 
   /**
-   * @brief Local path velocity callback function
-   * @param [in] msg Msg from local_path_velocity_sub_
+   * @brief Cmd vel callback function
+   * @param [in] msg Msg from
    */
-  void local_path_velocity_callback(const geometry_msgs::TwistConstPtr &msg);
+  void local_path_cmd_vel_callback(const geometry_msgs::TwistConstPtr &msg);
 
   /**
    * @brief Odom callback function
    * @param [in] msg Msg from odom_sub_
    */
-  void odom_callback(const nav_msgs::OdometryConstPtr &msg);
+  void odom_callback(const nav_msgs::OdometryConstPtr &msg) { odom_vel_ = msg->twist.twist; }
 
   /**
    * @brief Rear laser callback function
-   * @details Cache the closest valid sensor data and its index in rear_min_range and rear_min_idx respectively
    * @param [in] msg Msg from rear_laser_sub_
    */
   void rear_laser_callback(const sensor_msgs::LaserScanConstPtr &msg);
@@ -135,7 +129,7 @@ private:
    * @brief Recovery mode flag callback function
    * @param [in] msg Msg from recovery_mode_flag_sub_
    */
-  void recovery_mode_flag_callback(const std_msgs::Bool::ConstPtr &msg);
+  void recovery_mode_flag_callback(const std_msgs::Bool::ConstPtr &msg) { params_of_recovery_.use = msg->data; }
 
   /**
    * @brief Task stop flag callback function
@@ -145,12 +139,12 @@ private:
   void task_stop_flag_callback(const std_msgs::BoolConstPtr &msg);
 
   /**
-   * @brief Publish velocity function
+   * @brief Cache the closest valid sensor data and its index
    * @param [in] laser Laser data
-   * @param [in] min_idx Index of the closest valid sensor data
    * @param [in] min_range Closest valid sensor data
+   * @param [in] index_of_min_range Index of the closest valid sensor data
    */
-  void search_min_range(const sensor_msgs::LaserScan &laser, int &min_idx, float &min_range);
+  void search_min_range(const sensor_msgs::LaserScan &laser, float &min_range, int &index_of_min_range);
 
   /**
    * @brief Publish velocity function
@@ -165,7 +159,7 @@ private:
    * @brief Recovery mode function
    * @details Run when stuck is detected. Move away from the nearest obstacle. Face the direction in which the
    *   LocalPlanner is comfortable moving.
-   * @param [out] cmd_vel Velocity
+   * @param [out] cmd_vel Command velocity
    * @param [in] go_back Direction of motion
    */
   void recovery_mode(geometry_msgs::Twist &cmd_vel);
@@ -185,17 +179,18 @@ private:
    * @param [in] sim_time Simulation time
    * @return std::pair<float, float> {predicted_x, predicted_y}
    */
-  std::pair<float, float> sim_by_uniform_circluar_motion(const float &velocity, const float &yawrate, const float &sim_time);
+  std::pair<float, float>
+  sim_by_uniform_circluar_motion(const float &velocity, const float &yawrate, const float &sim_time);
 
   /**
-   * @brief Publish velocity function
-   * @param [in] cmd_vel Velocity
+   * @brief Publish Cmd Vel function
+   * @param [in] cmd_vel Command velocity
    */
   void publish_cmd_vel(geometry_msgs::Twist cmd_vel);
 
   /**
    * @brief Print status function
-   * @param [in] cmd_vel Velocity
+   * @param [in] cmd_vel Command velocity
    */
   void print_status(const geometry_msgs::Twist &cmd_vel);
 
@@ -211,18 +206,17 @@ private:
   ros::NodeHandle nh_;
   ros::NodeHandle private_nh_;
   ros::Publisher intersection_flag_pub_;
-  ros::Publisher velocity_pub_;
+  ros::Publisher cmd_vel_pub_;
   ros::Subscriber emergency_stop_flag_sub_;
   ros::Subscriber front_laser_sub_;
   ros::Subscriber joy_sub_;
-  ros::Subscriber local_path_velocity_sub_;
+  ros::Subscriber local_path_cmd_vel_sub_;
   ros::Subscriber odom_sub_;
   ros::Subscriber rear_laser_sub_;
   ros::Subscriber recovery_mode_flag_sub_;
   ros::Subscriber task_stop_flag_sub_;
 
   geometry_msgs::Twist cmd_vel_;
-  geometry_msgs::Twist joy_vel_;
   geometry_msgs::Twist odom_vel_;
   std::optional<sensor_msgs::LaserScan> front_laser_;
   std::optional<sensor_msgs::LaserScan> rear_laser_;
--- src/motion_decision.cpp
+++ src/motion_decision.cpp
@@ -5,20 +5,22 @@
  * @copyright Copyright (c) 2024
  */
 
+#include <algorithm>
 #include <string>
+#include <utility>
 
 #include "motion_decision/motion_decision.h"
 
 MotionDecision::MotionDecision(void) : private_nh_("~")
 {
   intersection_flag_pub_ = nh_.advertise<std_msgs::Bool>("/intersection_flag", 1, true);
-  velocity_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1, true);
+  cmd_vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1, true);
 
   emergency_stop_flag_sub_ = nh_.subscribe("/emergency_stop", 1, &MotionDecision::emergency_stop_flag_callback, this);
   front_laser_sub_ = nh_.subscribe("/front_laser/scan", 1, &MotionDecision::front_laser_callback, this);
   joy_sub_ = nh_.subscribe("/joy", 1, &MotionDecision::joy_callback, this);
-  local_path_velocity_sub_ =
-      nh_.subscribe("/local_path/cmd_vel", 1, &MotionDecision::local_path_velocity_callback, this);
+  local_path_cmd_vel_sub_ =
+      nh_.subscribe("/local_path/cmd_vel", 1, &MotionDecision::local_path_cmd_vel_callback, this);
   odom_sub_ = nh_.subscribe("/odom", 1, &MotionDecision::odom_callback, this);
   rear_laser_sub_ = nh_.subscribe("/rear_laser/scan", 1, &MotionDecision::rear_laser_callback, this);
   recovery_mode_flag_sub_ = nh_.subscribe("/recovery_mode_flag", 1, &MotionDecision::recovery_mode_flag_callback, this);
@@ -31,34 +33,30 @@ void MotionDecision::load_params(void)
 {
   // MotionDecisionParams
   private_nh_.param<int>("hz", params_.hz, 20);
-  private_nh_.param<int>("recovery_mode_threshold", params_.recovery_mode_threshold, 60);
-  private_nh_.param<int>("trigger_count_threshold", params_.trigger_count_threshold, 2);
-  private_nh_.param<float>("max_speed", params_.max_speed, 1.0);
+  private_nh_.param<float>("max_velocity", params_.max_velocity, 1.0);
   private_nh_.param<float>("max_yawrate", params_.max_yawrate, 1.0);
   private_nh_.param<float>("dt", params_.dt, 0.1);
   private_nh_.param<float>("predict_time", params_.predict_time, 1.0);
   private_nh_.param<float>("collision_distance", params_.collision_distance, 0.4);
   private_nh_.param<float>("safety_collision_time", params_.safety_collision_time, 0.5);
+  private_nh_.param<float>("stuck_time_threshold", params_.stuck_time_threshold, 1.0);
   private_nh_.param<std::string>("stop_sound_path", params_.stop_sound_path, std::string(""));
   private_nh_.param<std::string>("recovery_sound_path", params_.recovery_sound_path, std::string(""));
   private_nh_.param<std::string>("task_stop_sound_path", params_.task_stop_sound_path, std::string(""));
 
   // RecoveryParams
+  private_nh_.param<bool>("recovery/use", params_of_recovery_.use, true);
   private_nh_.param<float>("recovery/max_velocity", params_of_recovery_.max_velocity, 0.3);
   private_nh_.param<float>("recovery/max_yawrate", params_of_recovery_.max_yawrate, 0.3);
   private_nh_.param<float>("recovery/velocity_resolution", params_of_recovery_.velocity_resolution, 0.1);
   private_nh_.param<float>("recovery/yawrate_resolution", params_of_recovery_.yawrate_resolution, 0.1);
-}
-
-void MotionDecision::emergency_stop_flag_callback(const std_msgs::BoolConstPtr &msg)
-{
-  flags_.emergency_stop = msg->data;
+  private_nh_.param<float>("recovery/time", params_of_recovery_.time, 3.0);
 }
 
 void MotionDecision::front_laser_callback(const sensor_msgs::LaserScanConstPtr &msg)
 {
   front_laser_ = *msg;
-  search_min_range(front_laser_.value(), laser_info_.front_min_idx, laser_info_.front_min_range);
+  search_min_range(front_laser_.value(), laser_info_.front_min_range, laser_info_.front_index_of_min_range);
 }
 
 void MotionDecision::joy_callback(const sensor_msgs::JoyConstPtr &msg)
@@ -67,7 +65,7 @@ void MotionDecision::joy_callback(const sensor_msgs::JoyConstPtr &msg)
 
   if (mode_.first == "move" && mode_.second == "manual")
   {
-    cmd_vel_.linear.x = msg->buttons[4] ? msg->axes[1] * params_.max_speed : 0.0;
+    cmd_vel_.linear.x = msg->buttons[4] ? msg->axes[1] * params_.max_velocity : 0.0;
     cmd_vel_.angular.z = msg->buttons[4] ? msg->axes[0] * params_.max_yawrate : 0.0;
   }
   else if (mode_.first == "move" && mode_.second == "auto" && !flags_.local_path_received)
@@ -75,65 +73,61 @@ void MotionDecision::joy_callback(const sensor_msgs::JoyConstPtr &msg)
     cmd_vel_ = geometry_msgs::Twist();
   }
 
-  if(msg->buttons[11] && msg->buttons[12])
+  if (msg->buttons[11] && msg->buttons[12])
   {
     std_msgs::Bool flag;
     flag.data = true;
+    flags_.intersection = true;
     intersection_flag_pub_.publish(flag);
-    std::cout << "=========intersection=============" << std::endl;
+  }
+  else
+  {
+    flags_.intersection = false;
   }
 }
 
-void MotionDecision::local_path_velocity_callback(const geometry_msgs::TwistConstPtr &msg)
+void MotionDecision::local_path_cmd_vel_callback(const geometry_msgs::TwistConstPtr &msg)
 {
   if (mode_.first == "move" && mode_.second == "auto")
     cmd_vel_ = *msg;
   flags_.local_path_received = true;
 }
 
-void MotionDecision::odom_callback(const nav_msgs::OdometryConstPtr &msg) { odom_vel_ = msg->twist.twist; }
-
 void MotionDecision::rear_laser_callback(const sensor_msgs::LaserScanConstPtr &msg)
 {
   rear_laser_ = *msg;
-  search_min_range(rear_laser_.value(), laser_info_.rear_min_idx, laser_info_.rear_min_range);
-}
-
-void MotionDecision::recovery_mode_flag_callback(const std_msgs::Bool::ConstPtr &msg)
-{
-  flags_.enable_recovery_mode = msg->data;
+  search_min_range(rear_laser_.value(), laser_info_.rear_min_range, laser_info_.rear_index_of_min_range);
 }
 
 void MotionDecision::task_stop_flag_callback(const std_msgs::BoolConstPtr &msg)
 {
-  std_msgs::Bool flag = *msg;
-  if (flag.data)
+  flags_.task_stop = msg->data;
+  if (flags_.task_stop)
   {
-    std::cout << "========= task stop =========" << std::endl;
     if (params_.task_stop_sound_path != "")
     {
       std::string sound_command = "aplay " + params_.task_stop_sound_path + " &";
       system(sound_command.c_str());
       system(sound_command.c_str());
     }
-    flags_.move_mode = false;
+    mode_.first = "stop";
   }
   else
   {
-    flags_.move_mode = true;
+    mode_.first = "move";
   }
 }
 
-void MotionDecision::search_min_range(const sensor_msgs::LaserScan &laser, int &min_idx, float &min_range)
+void MotionDecision::search_min_range(const sensor_msgs::LaserScan &laser, float &min_range, int &index_of_min_range)
 {
   min_range = laser.range_max;
-  min_idx = 0;
+  index_of_min_range = 0;
   for (size_t i = 0; i < laser.ranges.size(); i++)
   {
     if (laser.ranges[i] < min_range)
     {
       min_range = laser.ranges[i];
-      min_idx = i;
+      index_of_min_range = i;
     }
   }
 }
@@ -159,45 +153,46 @@ MotionDecision::select_mode(const sensor_msgs::Joy::ConstPtr &joy, const std::pa
 void MotionDecision::process(void)
 {
   ros::Rate loop_rate(params_.hz);
+  const int max_recovery_count = static_cast<int>(params_of_recovery_.time * params_.hz);
+  const int max_stuck_count = static_cast<int>(params_.stuck_time_threshold * params_.hz);
   while (ros::ok())
   {
     // Detect stuck and recover for a certain period of time
-    if (mode_.first == "move" && mode_.second == "auto" && flags_.enable_recovery_mode)
+    if (mode_.first == "move" && mode_.second == "auto" && params_of_recovery_.use)
     {
-      if (0 < counters_.trigger && counters_.trigger < params_.trigger_count_threshold)
+      if (0 < counters_.recovery && counters_.recovery < max_recovery_count)
       {
         recovery_mode(cmd_vel_);
       }
-      else if (params_.trigger_count_threshold <= counters_.trigger)
+      else if (max_recovery_count <= counters_.recovery)
       {
         counters_.stuck = 0;
-        counters_.trigger = 0;
+        counters_.recovery = 0;
         if (!flags_.local_path_received)
           cmd_vel_ = geometry_msgs::Twist();
       }
       else if (((cmd_vel_.linear.x < DBL_EPSILON && fabs(cmd_vel_.angular.z) < DBL_EPSILON) ||
-       (odom_vel_.linear.x < 0.01 && fabs(odom_vel_.angular.z) < 0.01)))
+                (odom_vel_.linear.x < 0.01 && fabs(odom_vel_.angular.z) < 0.01)))
       {
-        if (counters_.stuck < params_.recovery_mode_threshold)
+        if (counters_.stuck < max_stuck_count)
           counters_.stuck++;
         else
-          counters_.trigger++;
+          counters_.recovery++;
       }
     }
     else if (mode_.second == "manual")
     {
       counters_.stuck = 0;
-      counters_.trigger = 0;
+      counters_.recovery = 0;
     }
 
     publish_cmd_vel(cmd_vel_);
 
-    laser_info_.front_min_range = -1.0;
-    laser_info_.rear_min_range = -1.0;
     front_laser_.reset();
     rear_laser_.reset();
+    laser_info_.front_min_range = -1.0;
+    laser_info_.rear_min_range = -1.0;
     flags_.local_path_received = false;
-    flags_.recovery_mode = false;
 
     loop_rate.sleep();
     ros::spinOnce();
@@ -206,28 +201,29 @@ void MotionDecision::process(void)
 
 void MotionDecision::recovery_mode(geometry_msgs::Twist &cmd_vel)
 {
-  counters_.trigger++;
-  flags_.recovery_mode = true;
+  counters_.recovery++;
   if (!front_laser_.has_value() || !rear_laser_.has_value())
     return;
 
   // select data by direction of motion
   const bool sim_back = 0.0 < cmd_vel.linear.x;
   const sensor_msgs::LaserScan laser = sim_back ? front_laser_.value() : rear_laser_.value();
-  const int min_idx = sim_back ? laser_info_.front_min_idx : laser_info_.rear_min_idx;
+  const int index_of_min_range = sim_back ? laser_info_.front_index_of_min_range : laser_info_.rear_index_of_min_range;
   const float max_velocity = sim_back ? -params_of_recovery_.max_velocity : params_of_recovery_.max_velocity;
-  const float velocity_resolution = sim_back ? -params_of_recovery_.velocity_resolution : params_of_recovery_.velocity_resolution;
+  const float velocity_resolution =
+      sim_back ? -params_of_recovery_.velocity_resolution : params_of_recovery_.velocity_resolution;
 
   // set cmd_vel to move away from the nearest obstacle
   float max_ttc = 0.0;
   cmd_vel.linear.x = 0.0;
-  cmd_vel.angular.z = min_idx < laser.ranges.size() / 2 ? 0.2 : -0.2;
+  cmd_vel.angular.z = index_of_min_range < laser.ranges.size() / 2 ? 0.2 : -0.2;
   cmd_vel.linear.z *= sim_back ? 1.0 : -1.0;
-  const float obs_angle = laser.angle_min + min_idx * laser.angle_increment;
+  const float obs_angle = laser.angle_min + index_of_min_range * laser.angle_increment;
   const bool obs_found_in_direction_of_travel = obs_angle < M_PI / 2.0;
   for (float velocity = velocity_resolution; fabs(velocity) <= fabs(max_velocity); velocity += velocity_resolution)
   {
-    for (float yawrate = -params_of_recovery_.max_yawrate; yawrate <= params_of_recovery_.max_yawrate; yawrate += params_of_recovery_.yawrate_resolution)
+    for (float yawrate = -params_of_recovery_.max_yawrate; yawrate <= params_of_recovery_.max_yawrate;
+         yawrate += params_of_recovery_.yawrate_resolution)
     {
       const float ttc = calc_ttc(velocity, yawrate, laser);
       if (ttc > max_ttc && ttc > params_.safety_collision_time)
@@ -238,14 +234,17 @@ void MotionDecision::recovery_mode(geometry_msgs::Twist &cmd_vel)
       }
       else if (ttc == max_ttc && ttc > params_.safety_collision_time)
       {
-        const std::pair<float, float> best_sim_pos = sim_by_uniform_circluar_motion(cmd_vel.linear.x, cmd_vel.angular.z, ttc);
+        const std::pair<float, float> best_sim_pos =
+            sim_by_uniform_circluar_motion(cmd_vel.linear.x, cmd_vel.angular.z, ttc);
         const std::pair<float, float> tmp_sim_pos = sim_by_uniform_circluar_motion(velocity, yawrate, ttc);
-        const float angle_between_best_sim_pos_and_obs = fabs(obs_angle - atan2(best_sim_pos.second, best_sim_pos.first));
+        const float angle_between_best_sim_pos_and_obs =
+            fabs(obs_angle - atan2(best_sim_pos.second, best_sim_pos.first));
         const float angle_between_tmp_sim_pos_and_obs = fabs(obs_angle - atan2(tmp_sim_pos.second, tmp_sim_pos.first));
 
         // if the angle between the obstacle and the robot is greater than 90 degrees, the robot will move backwards
-        if(obs_found_in_direction_of_travel && angle_between_best_sim_pos_and_obs > angle_between_tmp_sim_pos_and_obs
-         || !obs_found_in_direction_of_travel && angle_between_best_sim_pos_and_obs < angle_between_tmp_sim_pos_and_obs)
+        if (obs_found_in_direction_of_travel &&
+                angle_between_best_sim_pos_and_obs > angle_between_tmp_sim_pos_and_obs ||
+            !obs_found_in_direction_of_travel && angle_between_best_sim_pos_and_obs < angle_between_tmp_sim_pos_and_obs)
         {
           cmd_vel.linear.x = velocity;
           cmd_vel.angular.z = yawrate;
@@ -284,7 +283,8 @@ float MotionDecision::calc_ttc(const float &velocity, const float &yawrate, cons
   return ttc;
 }
 
-std::pair<float, float> MotionDecision::sim_by_uniform_circluar_motion(const float &velocity, const float &yawrate, const float &sim_time)
+std::pair<float, float>
+MotionDecision::sim_by_uniform_circluar_motion(const float &velocity, const float &yawrate, const float &sim_time)
 {
   const float radius = velocity / yawrate;
   const float theta = yawrate * sim_time;
@@ -296,26 +296,34 @@ std::pair<float, float> MotionDecision::sim_by_uniform_circluar_motion(const flo
 
 void MotionDecision::publish_cmd_vel(geometry_msgs::Twist cmd_vel)
 {
-  cmd_vel.linear.x = 0.0 < cmd_vel.linear.x ? std::min(cmd_vel.linear.x, static_cast<double>(params_.max_speed)) : std::max(cmd_vel.linear.x, static_cast<double>(-params_.max_speed));
-  cmd_vel.angular.z = 0.0 < cmd_vel.angular.z ? std::min(cmd_vel.angular.z, static_cast<double>(params_.max_yawrate)) : std::max(cmd_vel.angular.z, -static_cast<double>(params_.max_yawrate));
+  cmd_vel.linear.x = 0.0 < cmd_vel.linear.x ? std::min(cmd_vel.linear.x, static_cast<double>(params_.max_velocity))
+                                            : std::max(cmd_vel.linear.x, static_cast<double>(-params_.max_velocity));
+  cmd_vel.angular.z = 0.0 < cmd_vel.angular.z ? std::min(cmd_vel.angular.z, static_cast<double>(params_.max_yawrate))
+                                              : std::max(cmd_vel.angular.z, -static_cast<double>(params_.max_yawrate));
 
   if (flags_.emergency_stop || mode_.first == "stop")
     cmd_vel = geometry_msgs::Twist();
   if (mode_.first == "move" && mode_.second == "auto" && (!front_laser_.has_value() || !rear_laser_.has_value()))
     cmd_vel = geometry_msgs::Twist();
 
-  velocity_pub_.publish(cmd_vel);
+  cmd_vel_pub_.publish(cmd_vel);
   print_status(cmd_vel);
 }
 
 void MotionDecision::print_status(const geometry_msgs::Twist &cmd_vel)
 {
   std::cout << "=== " << mode_.second << " (" << mode_.first << ") ===" << std::endl;
-  if (flags_.recovery_mode)
+  if (0 < counters_.recovery)
   {
     std::cout << "#####################" << std::endl;
     std::cout << "### recovery mode ###" << std::endl;
     std::cout << "#####################" << std::endl;
+  }
+  else if (0 < counters_.stuck)
+  {
+    std::cout << "################" << std::endl;
+    std::cout << "### stuck!!! ###" << std::endl;
+    std::cout << "################" << std::endl;
   }
   if (flags_.emergency_stop)
   {
@@ -323,11 +331,23 @@ void MotionDecision::print_status(const geometry_msgs::Twist &cmd_vel)
     std::cout << "### emergency stop ###" << std::endl;
     std::cout << "######################" << std::endl;
   }
+  if (flags_.task_stop)
+  {
+    std::cout << "#################" << std::endl;
+    std::cout << "### task stop ###" << std::endl;
+    std::cout << "#################" << std::endl;
+  }
+  if (flags_.intersection)
+  {
+    std::cout << "####################" << std::endl;
+    std::cout << "### intersection ###" << std::endl;
+    std::cout << "####################" << std::endl;
+  }
   std::cout << "min front laser : " << laser_info_.front_min_range << std::endl;
   std::cout << "min rear laser  : " << laser_info_.rear_min_range << std::endl;
-  std::cout << "trigger count   : " << counters_.trigger << std::endl;
+  std::cout << "recovery time   : " << counters_.recovery / static_cast<float>(params_.hz) << std::endl;
   if (0 < counters_.stuck)
-    std::cout << "stuck count : " << counters_.stuck << std::endl;
+    std::cout << "stuck time      : " << counters_.stuck / static_cast<float>(params_.hz) << std::endl;
   if (mode_.first == "move")
   {
     std::cout << "cmd vel :" << std::endl;
